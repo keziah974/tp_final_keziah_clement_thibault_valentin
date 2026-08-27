cd68b5a947c50e9148a3b5f853f50772d607547a
keeeeziah <keziahnoamperfillon@gmail.com>
2026-08-27 12:06:33 +0200

    initialisation de la structure et configuration de base

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('meteo-VCTK');
+  title = 'Weather App';
 }
